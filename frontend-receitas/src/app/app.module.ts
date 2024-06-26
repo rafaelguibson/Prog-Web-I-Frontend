23dafafd0a413aa822c7746ecfa4f65719055389
Rafael Guibson <rafaelguibsons@gmail.com>
2024-06-26 09:03:27 -0300

    Adicionado sidenav inicial e configurado imports angular material

--- frontend-receitas/src/app/app.module.ts
+++ frontend-receitas/src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomSidenavComponent } from './components/custom-sidenav/custom-sidenav.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatSidenav, MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatCard, MatCardContent, MatCardFooter} from "@angular/material/card";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatListItem, MatListModule, MatNavList} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomSidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavContainer,
+    MatSidenav,
+    MatCard,
+    MatCardContent,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    MatToolbar,
+    MatNavList,
+    MatListItem,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardFooter,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
